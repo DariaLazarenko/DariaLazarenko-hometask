59c820138d1d26529b153ef98e8621d4838125b2
Дарья Лазаренко <dlworkonly@gmail.com>
2021-08-19 00:49:40 +0300

    Тестирование 34 задания одномерных массивов, есть вопросы

---
+++ Arrays/ArrTasks.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arrays
+{
+    public class ArrTasks
+    {
+        //Для заданий по тестированию
+        public int Test34arr(int[] array) 
+        {
+            //Дан массив вещественных чисел, размер которого N.
+            //Подсчитать, сколько в нем отрицательных, положительных и нулевых элементов.
+            //public Tuple <int, int, int> Test34arr(int[] array)
+
+
+            //int sumZ = 0;
+            int sumPos = 0;
+            //int sumNeg = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                //if (array[i] == 0)
+                //{
+                //    sumZ++;
+                //}
+                //else if (array[i] < 0)
+                //{
+                //    sumNeg++;
+                //}
+                if (array[i] > 0)
+                {
+                    sumPos++;
+                }
+
+            }
+            //return Tuple.Create(sumPos, sumNeg, sumZ);
+            return sumPos;
+        }
+
+    }
+}
---
+++ ArraysTests1/ArrTasksTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Arrays;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arrays.Tests
+{
+    [TestClass()]
+    public class ArrTasksTests
+    {
+        [TestMethod()]
+        public void Test34arrTest()
+        {
+            //Arrange
+            int[] arr1 = { 0, 3, -5, 7, -8 };
+
+            ArrTasks task = new ArrTasks();
+
+            int expPos = 2;
+            //int expNeg = 2;
+            //int expZ = 1;
+
+            //Act
+            int actPos = task.Test34arr(arr1);
+
+            //Assert
+            Assert.AreEqual(expPos, actPos);
+        }
+    }
+}
